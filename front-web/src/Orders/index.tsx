aefc72d169f1707de0ea379c50d3fdaad180b509
JonathanComarella <jonathanwcomarella@gmail.com>
2021-10-27 20:24:13 -0300

    Initial Routes

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import { ReactComponent as Logo } from './logo.svg';
+
+function Orders() {
+    return (
+        <div>
+            Orders
+        </div>
+    )
+}
+
+export default Orders;
